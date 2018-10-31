2fc5e312b04b3bbf145f9504fea6dab24531b121
Jitendra Sahu <jitendra.kumar@in.accemy.com>
2018-10-31 18:57:41 +0530

    Project Addes

---
+++ src/main/java/com/spring/demo/model/Employee.java
@@ -0,0 +1,45 @@
+package com.spring.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue
+	private int employeeId;
+	private String employeeName;
+	private String technology;
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getTechnology() {
+		return technology;
+	}
+
+	public void setTechnology(String technology) {
+		this.technology = technology;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", technology=" + technology
+				+ "]";
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into Employee values(101,'Jeetendra','JAVA');
+insert into Employee values(102,'Ankit','PHP');
+insert into Employee values(103,'Rajdev','JAVA');
+insert into Employee values(104,'Sandeep','Dotnet');
+insert into Employee values(105,'Subham','JAVA');
+insert into Employee values(106,'Madhusudhan','Spring');
+insert into Employee values(107,'Anurag','JAVA');
